fea31c703b265d640d1a97d338403c74ddb8403b
kauanfalmeida-maker <kauan.f.almeida@edu.senai.br>
2026-06-12 09:00:48 -0300

    Pressione enter para parar

--- Nave.py
+++ Nave.py
@@ -1,71 +1,73 @@
-## Definir as variáveis
+##Definir as variaveis
 combustivel = 100
 tripulantes = []
 
-## Definir funções
+##Definir funcoes:
 
 def viajar():
-    ## Aqui vamos gatar combustível
-    global combustivel ## Avisa função que vamos modificar uma variavel externa
-    if(combustivel>=30):
-      combustivel = combustivel - 30 
-      print("A nave viajou")
-
+    ##aqui vamos gastar combustivel
+    global combustivel ##Avisa que vamos modificar uma variavel externa
+    if (len(tripulantes)==0):
+        print("Não possui tripulantes na nave, proibido viajar!")
     else:
-       print("Você está sem combustivel suficiente. Abasteça!")
+        if(combustivel>=30):
+            combustivel = combustivel - 30
+            print("A nave viajou")
+        else:
+        
+            print("Você está sem combustivel suficiente.Abasteça!")
+    travarMenu()
 
 def abastecer():
     global combustivel
     combustivel = 100
-    print("Tanque cheio! ⛽")
-
+    print("Tanque cheio!")
+    travarMenu()
+    
 def status_nave():
-    ## Mostre a quantidade de combustivel e os tripulantes
-    print("\n------STATUS DA NAVE------")
-    print(f"Temos {combustivel} de combustível")
-    print(f"Os tripulantes são: {tripulantes}")
-    print("---------------------------\n")
+    print("----- STATUS DA NAVE -----")
+    print(f"Temos {combustivel} de cumbustivel")
+    print(f"Os tripulantes sao: {tripulantes}")
+    print("-------------------------- \n")
+    travarMenu()
 
 def registrarTripulante():
-    ## Essa função pergunta o nome do tripulante e adiciona na lista de tripulantes
-    novotripulante = input("Qual o nome do novo tripulante?: ") #Pergunta quem
-    tripulantes.append(novotripulante) #inserimos o fulaninho
-    print("Tripulante inserido com sucesso! 🚀")
+    ##Essa funcao pergunta o nome do tripulante e ad0iciona na lista de tripulantes
+    novotripulante = input("Qual o nome do novo tripulante?: ")
+    tripulantes.append(novotripulante)
+    print("Tripulante inserido com sucesso! ")
+    travarMenu()
 
-## Criar um menu
+## Cria uma função que tira o ultimo tripulante
+def tirar():
+    tripulantes.pop()
+    print("Tripulante retirado")
+    travarMenu()
 
-while True:  ## Esse loop roda para sempre!
-    print("Bem vindo ao menu interativo da nave. Por favor selecione uma opção:")
-    print("\n1- Mostrar status da nave | 2- Viajar | 3-  Abastecer | 4- novo tripulante | 5- Sair")
+## Criar uma função para pausar o código entre as interações do usuário
+def travarMenu():
+    ## Nosso código vai aqui
+    input("\nPressione <ENTER> para continuar....")
+
+## criar um menu
+
+while True:
+    print("\nBem vindo ao menu interativo da nave. Por favor selecione uma opção:")
+    print("\n1- Mostrar status da nave| 2- Viajar | 3- Abastecer | 4- Novo Tripulante | 5- Sair | 6- Tirar")
     opcao = input("Escolha: ")
-    if(opcao == "1"):
-       status_nave()
+    if (opcao == "1"):
+        status_nave()
     elif (opcao == "2"):
         viajar()
-    elif (opcao == "3"):
+    elif (opcao =="3"):
         abastecer()
-    elif (opcao == "4"):
+    elif (opcao =="4"):
         registrarTripulante()
-    elif (opcao == "5"):
+    elif (opcao =="5"):
         print("Viagem encerrada!")
         break
-
-
-
-
-
-
-# status_nave()
-# registrarTripulante()
-# status_nave()
-
-
-# status_nave()
-# viajar()
-# viajar()
-# status_nave()
-# viajar()
-# viajar()
-# abastecer()
-# viajar()
-# status_nave()
+    elif (opcao =="6"):
+        if len(tripulantes) ==0:
+            print("Nenhum tripulante foi removido, pois não há tripulantes")
+        else:
+            tirar()
